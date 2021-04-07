dab35e6c2f7fd50a87e6e9153d797c389fed47e2
SYAUQIZAIDAN KHAIRAN KHALAF <syaokay@gmail.com>
2021-04-08 00:13:03 +0700

    Adding database connection

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL = parse_url('postgres://sbfjaxroydyttc:418135d149b7ac63af79565e75ab3aab95756e11bec244c094b6f9ed42481549@ec2-3-91-127-228.compute-1.amazonaws.com:5432/d6gvqkhh3jtsrm');
+
 return [
 
     /*
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +67,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => 'postgres://sbfjaxroydyttc:418135d149b7ac63af79565e75ab3aab95756e11bec244c094b6f9ed42481549@ec2-3-91-127-228.compute-1.amazonaws.com:5432/d6gvqkhh3jtsrm',
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["host"],
+            'database' => ltrim($DATABASE_URL["path"], '/'),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
@@ -123,7 +125,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
